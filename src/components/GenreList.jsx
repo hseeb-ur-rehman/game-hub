48425b53cee25101dfbee16a6a36844b26e61e70
Haseeb ur Rehman <sp20-bcs-127@cuilahore.edu.pk>
2024-04-25 11:31:55 +0500

    genre list

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -1,13 +1,15 @@
-import { Button, HStack, Image, List, ListItem, Text } from '@chakra-ui/react'
+import { Button, Heading, HStack, Image, List, ListItem, Text } from '@chakra-ui/react'
 import useGenre from '../hooks/useGenre'
 import LooadingSkeletonGenre from './LooadingSkeletonGenre'
 import { useState } from 'react'
 
-const GenreList = ({ setSelectedGenre , selectedGenre}) => {
+const GenreList = ({ setSelectedGenre, selectedGenre }) => {
   const { genre, isLoading } = useGenre()
-  
+
   const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   return (
+    <>
+    <Heading as='h1' fontSize='4xl' marginBottom={7}>Genres</Heading>
     <List spacing={3}>
       {isLoading &&
         skeleton.map((skeleton) => <LooadingSkeletonGenre key={skeleton} />)}
@@ -17,15 +19,26 @@ const GenreList = ({ setSelectedGenre , selectedGenre}) => {
             <Image
               borderRadius={8}
               boxSize="56px"
+              objectFit="cover"
               src={genre.image_background}
             />
-            <Button padding={2} fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => setSelectedGenre(genre)} variant="link" paddingX={5} fontSize="lg">
+            <Button
+              whiteSpace="normal"
+              textAlign="left"
+              padding={2}
+              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
+              onClick={() => setSelectedGenre(genre)}
+              variant="link"
+              paddingX={5}
+              fontSize="lg"
+            >
               {genre.name}
             </Button>
           </HStack>
         </ListItem>
       ))}
     </List>
+    </>
   )
 }
 
